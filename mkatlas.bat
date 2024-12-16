c75c157bb9749f350e721e02f12ce13220b725c0
abab <abab@cc.com>
2024-12-16 16:49:57 +0800

    调整项目结构

---
+++ mkatlas.bat
@@ -0,0 +1,14 @@
+	
+cd /d %~dp0
+
+utils\pngs.exe
+
+for %%i in ("MYWD\images\*.xml") do move %%i %%i.a
+
+@REM 把mod工具加到path里，比如我的路径："D:\@apps\@games\SteamLibrary\steamapps\common\Don't Starve Mod Tools\mod_tools"
+cmd /c autocompiler
+
+for %%i in ("MYWD\images\*.xml") do move %%i.a %%i
+for %%i in ("MYWD\images\*.png") do del %%i
+
+for /r "MYWD\exported" %%i in ("*.zip") do del %%i

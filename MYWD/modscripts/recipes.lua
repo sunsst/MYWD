b91f38727687768534633627cbaeba56743aa88d
sunsst <169758532+sunsst@users.noreply.github.com>
2024-12-16 18:28:04 +0800

    制作配方模板

---
+++ MYWD/modscripts/recipes.lua
@@ -0,0 +1,24 @@
+-- 在这个文件添加制作配方，记得去chs文件添加名字
+
+
+-- 创建温蒂专属制作配方，都字面意思，不要哪个参数不填就行
+-- 比如：AddWendyRecipe("ghostlyelixir_moon", { Ingredient("moon_tree_blossom", 1) }, "mywd_wdga_1", "elixirbrewer")
+local function AddWendyRecipe(prefab, ingredients, builder_skill, builder_tag)
+    RegisterScrapbookIconAtlas("images/mywd_icon.xml", prefab .. ".tex")
+    AddCharacterRecipe(prefab, ingredients, GLOBAL.TECH.NONE, {
+            atlas = GLOBAL.GetScrapbookIconAtlas(prefab .. ".tex"),
+            builder_tag = builder_tag,
+            product = prefab,
+            builder_skill = builder_skill
+        },
+        { "CHARACTER" })
+end
+
+
+-- 示例：添加月亮药剂的配方
+AddWendyRecipe("ghostlyelixir_moon", {
+    Ingredient("moon_tree_blossom", 1),
+    Ingredient("moonbutterflywings", 1),
+    Ingredient("ghostflower", 1),
+    Ingredient("purebrilliance", 1),
+}, "mywd_wdga_1", "elixirbrewer")

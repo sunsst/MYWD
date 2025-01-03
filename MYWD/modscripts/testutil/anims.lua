f33446898266e6ce9de954d0a5882de90439357b
anim <anim@cc.com>
2025-01-03 16:45:04 +0800

    2025/01/03 周五 16:45:04.59

--- MYWD/modscripts/testutil/anims.lua
+++ MYWD/modscripts/testutil/anims.lua
@@ -1,6 +1,7 @@
 -- 第一层是动画文件名 build
 -- 第二层是动画分组名 bank
 -- 第三层是动画名 anim
+-- 不想用的可以不删直接注释掉就行
 MYWD_ANIMS = {
     ["ghost_abigail_build"] = {
         ["ghost"] = {
@@ -21,5 +22,25 @@ MYWD_ANIMS = {
             "idle_custom",
             "idlexxx",
         }
-    }
+    },
+    -- ["ghost_abigail_build"] = {
+    --     ["ghost"] = {
+    --         "idle",
+    --         "run_start",
+    --         "run",
+    --         "run_stop",
+    --         "angry",
+    --         "appear",
+    --         "dissipate",
+    --         "hit",
+    --         "shy",
+    --         "attack_loop",
+    --         "attack_pre",
+    --         "attack_pst",
+    --         "dance",
+    --         "flower_change",
+    --         "idle_custom",
+    --         "idlexxx",
+    --     }
+    -- }
 }
